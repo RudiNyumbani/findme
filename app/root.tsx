5f20cfdb44fd221064787857f462f1ae264a88f7
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2025-03-07 17:00:49 +0300

    Removing the navbar to keep layout clean

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  Link,
   Links,
   Meta,
   Outlet,
@@ -10,6 +9,8 @@ import { useEffect } from "react";
 import type { LinksFunction } from "@remix-run/node";
 
 import Footer from "~/components/footer"; // Import Footer
+import Navbar from "~/components/navbar"; // Import Navbar
+
 
 // Import Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -39,92 +40,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links /> {/* Remix will inject styles from `links()` here */}
       </head>
       <body className="bg-dark text-white d-flex flex-column min-vh-100">
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-lg p-3 mb-2 bg-white fixed-top">
-          <div className="container-fluid">
-            {/* Brand */}
-            <Link className="navbar-brand" to="#">FindMe</Link>
-
-            {/* Offcanvas Toggle Button */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasNavbar"
-              aria-controls="offcanvasNavbar"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            {/* Offcanvas Sidebar */}
-            <div
-              className="offcanvas offcanvas-end"
-              tabIndex={-1}
-              data-bs-scroll="true"
-              id="offcanvasNavbar"
-              aria-labelledby="offcanvasNavbarLabel"
-            >
-              <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">FindMe</h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
-                ></button>
-              </div>
-
-              <div className="offcanvas-body">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link active" to="/">Missing Persons</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/unidentified">Unidentified Persons</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/unclaimed">Unclaimed Persons</Link>
-                  </li>
-                </ul>
-
-                {/* New Report Button */}
-                <Link className="btn btn-primary me-3" to="#">New Report</Link>
-
-                {/* Language Dropdown, Register & Login */}
-                <ul className="navbar-nav">
-                  <li className="nav-item dropdown">
-                    <Link
-                      className="nav-link dropdown-toggle"
-                      to="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      Language
-                    </Link>
-                    <ul className="dropdown-menu">
-                      <li><Link className="dropdown-item" to="#">English</Link></li>
-                      <li><Link className="dropdown-item" to="#">Swahili</Link></li>
-                    </ul>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="#">Register</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="#">Login</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </nav>
-
+        
+        <Navbar />
 
         {/* Page Content */}
         <main className="flex-grow-1">
           {children}
         </main>
+
         <Footer />
 
         <ScrollRestoration />
