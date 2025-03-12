b8f275b86796899c990132aa1290c0a8ef3a5cac
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2025-03-12 17:22:49 +0300

    Updating the links on the navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -79,10 +79,10 @@ export default function Navbar() {
                 </ul>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="#">Register</Link>
+                <Link className="nav-link" to="/register">Register</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="#">Login</Link>
+                <Link className="nav-link" to="/login">Login</Link>
               </li>
             </ul>
           </div>
