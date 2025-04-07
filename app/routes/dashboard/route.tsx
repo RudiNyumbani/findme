7a87afee9b514b1df1cdb5bb283c574121c59b8a
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2025-04-07 14:46:23 +0300

    Creating the dash entry point

---
+++ app/routes/dashboard/route.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from '@remix-run/node';
+import { Outlet } from '@remix-run/react';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Dashboard' },
+    { name: 'description', content: 'Welcome to your dashboard' },
+  ];
+};
+
+export default function Test() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Test page layout for all /dashboard/* routes</h1>
+      <Outlet />
+    </div>
+  );
+}
