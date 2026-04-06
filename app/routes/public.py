448c209579b4dae28d3a0ca53d71680cc3d141d4
Mu-Gee <61751306+Mu-Gee@users.noreply.github.com>
2026-04-06 11:35:42 +0300

    Adding dynamic path for templates

--- app/routes/public.py
+++ app/routes/public.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 # Correctly initialize templates
-templates = Jinja2Templates(directory="c:/Users/james/findme/app/templates")
+BASE_DIR = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 router = APIRouter()
 
